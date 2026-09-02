94d3377ad3512d40b1ef8c8fbc270ac83b06f10b
Julia <jba516@uky.edu>
2026-09-02 13:00:04 -0400

    New

---
+++ Classwork/day_5/day5_files/folder_1/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# tells the system to run this file with bash
+echo "This is my first script."                                # print a message
+curl -o term_comm.txt https://winjapati.github.io/courses/301/term_comm.txt  # download the file
+mkdir folder_1                                                 # make a new folder
+mv term_comm.txt folder_1                                      # move it into folder_1
+echo term_comm.txt                                             # print the filename
