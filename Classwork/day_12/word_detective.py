a5620b2e04431afd81d8f67ed6f1da47389314d2
Julia <jba516@uky.edu>
2026-09-21 12:16:04 -0400

    yes

---
+++ Classwork/day_12/word_detective.py
@@ -0,0 +1,5 @@
+word = input("Enter a word: ")
+first_letter = word[0]
+is_vowel = (word[0] == "a" or word[0] == "e" or word[0] == "i" or word[0] == "o" or word[0] == "u")
+print(first_letter, is_vowel)
+    
